b623fb2a6e4a91ff4630c49c54898d240c4f3ec1
adonias <mulugeta.adonias@gmail.com>
2018-11-05 23:11:15 +0300

    done defining the account model

--- model/account-model.js
+++ model/account-model.js
@@ -1,8 +1,26 @@
 const mongoose = require("mongoose");
 
 const accountModel = new mongoose.Schema({
-    user       : { type : mongoose.ObjectId, ref : "User"},
-    media      : { type : mongoose.ObjectId, ref: "Media"},
-    created_at : { type : Date },
-    updated_at : { type : Date }
+    wallet_id     : { type : mongoose.ObjectId, ref : "wallet"},
+    transactions  : [{ type : mongoose.ObjectId, ref: "transaction"}],
+    created_at    : { type : Date },
+    updated_at    : { type : Date }
 });
+
+// database hooks
+// run this before every new record is saved
+accountModel.pre('save', function (next) {
+    let now = (new Date()).toISOString();
+    this.created_at = now;
+    this.updated_at = now;
+    next()
+});
+
+// run this before every update
+accountModel.pre('findOneAndUpdate', function(next){
+    this.update({},{ $set: { updated_at: new Date() } });
+    next()
+});
+
+//expose the model to the outside scripts
+exports = mongoose.model("account", accountModel);
