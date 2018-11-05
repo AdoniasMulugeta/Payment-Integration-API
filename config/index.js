5350ea0d4fb13613b162295e653cbc261e073938
adonias <mulugeta.adonias@gmail.com>
2018-11-05 22:19:08 +0300

    added some config strings

--- config/index.js
+++ config/index.js
@@ -1,13 +1,19 @@
 exports = {
-    DB_URL: "mongodb://localhost/",
-    DB_NAME: "audio-converter",
+    // mongodb database uri
+    DB_URL: "mongodb://localhost/payment-integration",
+
+    // node js server listening port
     SERVER_PORT: process.env.PORT || 3000,
+
+    //salt rounds factor for password hashing
     SALT_ROUNDS: 10,
-    JWT_SECRET : "z7m7NQgMAi7F",
-    //default media save path
-    MEDIA_PATH : './public/audio',
-    // Maximum Page Size
+
+    // random string secret used for jwt token generating
+    JWT_SECRET : process.env.JWT_SECRET || "z7m7NQgMAi7F",
+
+    // Maximum pagination Page Size
     MAX_PAGE_SIZE: 2,
-    // DEFAULT SORT FIELD,
+
+    // pagination default selected sorting field,
     DEFAULT_SORT: "date_updated"
 };
