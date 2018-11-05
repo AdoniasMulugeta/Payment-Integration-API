0ec2d97fd392301d03e178e6667fca23754c7c66
adonias <mulugeta.adonias@gmail.com>
2018-11-05 22:51:28 +0300

    done defining the transaction model

---
+++ model/transaction-model.js
@@ -0,0 +1,29 @@
+// import third-party modules
+const mongoose = require("mongoose");
+
+// define the schema for the model 
+const transactionModel = new mongoose.Schema({
+    from_wallet      : { type : mongoose.ObjectId, ref : "wallet"},
+    to_wallet        : { type : mongoose.ObjectId, ref : "wallet"},
+    transaction_date : { type: Date },
+    created_at       : { type : Date },
+    updated_at       : { type : Date }
+});
+
+// database hooks
+// run this before every new record is saved
+transactionModel.pre('save', function (next) {
+    let now = (new Date()).toISOString();
+    this.created_at = now;
+    this.updated_at = now;
+    next()
+});
+
+// run this before every update
+transactionModel.pre('findOneAndUpdate', function(next){
+    this.update({},{ $set: { updated_at: new Date() } });
+    next()
+});
+
+//expose the model to the outside scripts
+exports = mongoose.model("transaction", transactionModel);
