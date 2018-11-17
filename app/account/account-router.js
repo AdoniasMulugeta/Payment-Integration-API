2279b77bcda2e2576762ead4d8166c372ff0b8db
adonias <mulugeta.adonias@gmail.com>
2018-11-17 12:10:55 +0300

    reorganized the account router a bit

--- app/account/account-router.js
+++ app/account/account-router.js
@@ -4,19 +4,66 @@ const router = require("express").Router();
 //importing custom modules
 const accountController = require("./account-controller");
 const accountValidator  = require("./account-validator");
+const accessControl  = require("../auth/access-control")
 
 //define http routes
-router.get   ('/',                          accountController.getAccounts);
-router.get   ('/:account',                  accountValidator.checkAccount,  accountController.getAccount);
-router.get   ('/:account/balance',          accountValidator.checkAccount,  accountController.getBalance);
-router.get   ('/:account/transactions',     accountValidator.checkAccount,  accountController.getTransactions);
-router.get   ('/:account/transactions/:id', accountValidator.checkAccount,  accountController.getTransactions);
+router.get ('/',
+    accessControl.role.ADMIN_OR_USER,
+    accountController.getAccounts
+);
 
-router.post  ('/create',                           accountValidator.createAccount, accountController.createAccount);
-router.post  ('/:account/transfer',                accountValidator.checkAccount,  accountController.transfer);
-router.post  ('/:account/transactions/:id/refund', accountValidator.checkAccount,  accountController.refund);
+router.get ('/:account',
+    accessControl.role.ADMIN_OR_USER,
+    accountValidator.checkAccount,
+    accountController.getAccount
+);
 
-router.put    ('/:account', accountValidator.checkAccount, accountController.updateAccount);
-router.delete ('/:account', accountValidator.checkAccount, accountController.removeAccount);
+router.get ('/:account/balance',
+    accessControl.role.ADMIN_OR_USER,
+    accountValidator.checkAccount,
+    accountController.getBalance
+);
+
+router.get ('/:account/transactions',
+    accessControl.role.ADMIN_OR_USER,
+    accountValidator.checkAccount,
+    accountController.getTransactions
+);
+
+router.get ('/:account/transactions/:id',
+    accessControl.role.ADMIN_OR_USER,
+    accountValidator.checkAccount,
+    accountController.getTransactions
+);
+
+router.post ('/create',
+    accessControl.role.ADMIN_OR_USER,
+    accountValidator.createAccount,
+    accountController.createAccount
+);
+
+router.post ('/:account/transfer',
+    accessControl.role.ADMIN_OR_USER,
+    accountValidator.checkAccount,
+    accountController.transfer
+);
+
+router.post ('/:account/transactions/:id/refund',
+    accessControl.role.ADMIN_OR_USER,
+    accountValidator.checkAccount,
+    accountController.refund
+);
+
+router.put ('/:account',
+    accessControl.role.ADMIN_OR_USER,
+    accountValidator.checkAccount,
+    acountController.updateAccount
+);
+
+router.delete ('/:account',
+    accessControl.role.ADMIN_OR_USER,
+    accountValidator.checkAccount,
+    accountController.removeAccount
+);
 
 module.exports = router;
