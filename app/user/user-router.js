0785b1c6771114074ac8baec69df9ed5d260a184
adonias <mulugeta.adonias@gmail.com>
2018-11-17 12:48:32 +0300

    fixed some issues with error sending

--- app/user/user-router.js
+++ app/user/user-router.js
@@ -8,11 +8,34 @@ const authValidator  = require("../auth/auth-validator");
 
 //define http get request route
 
-router.get   ('/',       userController.getUsers);
-router.get   ('/:id',    userController.getUser);
-router.post  ('/signup', authValidator.signUp, authValidator.errorHandler, authController.signUp);
-router.post  ('/login',  authValidator.logIn, authValidator.errorHandler, authController.logIn);
-router.put   ('/:id',    authController.tokenValidator, userValidator.update, userValidator.errorHandler ,userController.updateUser);
-router.delete('/:id',    authController.tokenValidator,userController.removeUser);
+router.get ('/',
+    userController.getUsers
+);
+
+router.get ('/:id',
+    userController.getUser
+);
+
+router.post ('/signup',
+    authValidator.signUp,
+    authController.signUp
+);
+
+router.post ('/signin',
+    authValidator.signIn,
+    authController.signIn
+);
+
+router.put ('/:id',
+    authController.tokenValidator,
+    userValidator.update ,
+    userController.updateUser
+);
+
+router.delete ('/:id',
+    authController.tokenValidator,
+    userController.removeUser
+);
+
 
 module.exports = router;
