74bbbc4a5649532c930699426ac0d4bb358d0ef5
adonias <mulugeta.adonias@gmail.com>
2018-11-20 11:51:27 +0300

    made the account model an embedded document under user model

--- app/client/client-model.js
+++ app/client/client-model.js
@@ -3,15 +3,22 @@ const mongoose = require("mongoose");
 
 // importing custom models
 const CONFIG   = require("../../config");
-const accountModel  = require('../account/account-model');
 
+// define the schema for an account model to be embedded in user object
+const accountSchema = new mongoose.Schema({
+    FID        : { type : mongoose.ObjectId , ref: "FI", required: true},
+    balance    : { type : Number, required: true},
+    enabled    : { type : Boolean , default : true, required: true },
+    created_at : { type : Date , default: Date.now()},
+    updated_at : { type : Date , default: Date.now()}
+});
 
 // define the schema for the model
 const clientModel = new mongoose.Schema({
     full_name    : { type : String, required: true},
     phone_number : { type : Number, required: true},
-    user_id      : { type : mongoose.ObjectId , ref : "user"},
-    accounts     : [ accountModel ],
+    user_id      : { type : mongoose.ObjectId , ref : "user", required: true},
+    accounts     : [ accountSchema ],
     enabled      : { type : Boolean, default: true},
     created_at   : { type : Date },
     updated_at   : { type : Date }
