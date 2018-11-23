e5bc76354890393569070f39bb97df44dd366e2b
adonias <mulugeta.adonias@gmail.com>
2018-11-23 20:16:50 +0300

    added logic to check if resource is enabled before fetching it.

--- app/FI/FI-dal.js
+++ app/FI/FI-dal.js
@@ -1,16 +1,16 @@
 // importing custom modules
 const FIModel = require("./FI-model");
 
-exports.getFIById  = async id    => await FIModel.findById(id);
+exports.getFIById  = async id    => await FIModel.findById(id).where('enabled').equals(true);
 
-exports.getFI      = async query => await FIModel.findOne(query);
+exports.getFI      = async query => await FIModel.findOne(query).where('enabled').equals(true);
 
-exports.getFIs     = async query => await FIModel.find(query);
+exports.getFIs     = async query => await FIModel.find(query).where('enabled').equals(true);
 
 exports.createFI   = async data  => await FIModel.create(data);
 
-exports.updateFI   = async (id,data)  => await FIModel.findOneAndUpdate({_id: id},{$set:data},{new: true});
+exports.updateFI   = async (id,data)  => await FIModel.findOneAndUpdate({_id: id},{$set:data},{new: true}).where('enabled').equals(true);
 
-exports.removeFI   = async id  => await FIModel.findOneAndDelete({_id: id});
+exports.removeFI   = async id  => await FIModel.findOneAndUpdate({_id: id}, {enabled: false}).where('enabled').equals(true);
 
 
--- app/user/user-dal.js
+++ app/user/user-dal.js
@@ -1,18 +1,18 @@
 // importing custom modules
 const userModel = require("./user-model");
 
-exports.getUserById  = async id    => await userModel.findById(id,"-password");
+exports.getUserById  = async id    => await userModel.findById(id,"-password").where('enabled').equals(true);
 
-exports.getUser      = async query => await userModel.findOne(query);
+exports.getUser      = async query => await userModel.findOne(query).where('enabled').equals(true);
 
-exports.getUsers     = async query => await userModel.find(query);
+exports.getUsers     = async query => await userModel.find(query).where('enabled').equals(true);
 
 exports.createUser   = async data  => await userModel.create(data);
 
-exports.updateUser   = async (id,data)  => await userModel.findOneAndUpdate({_id: id},{$set:data},{new: true});
+exports.updateUser   = async (id,data)  => await userModel.findOneAndUpdate({_id: id},{$set:data},{new: true}).where('enabled').equals(true);;
 
-exports.removeUser   = async id  => await userModel.findOneAndDelete({_id: id});
+exports.removeUser   = async id  => await userModel.findOneAndUpdate({_id: id}, {enabled: false});
 
-exports.getUserWithPassword  = async query => await userModel.findOne(query).select('+password');
+exports.getUserWithPassword  = async query => await userModel.findOne(query).select('+password').where('enabled').equals(true);
 
 
